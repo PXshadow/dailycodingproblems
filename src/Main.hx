cc4097aa5dcb4f3502501ea285084ee9c4eebfb1
Joey <joeyprather8@gmail.com>
2019-10-29 23:03:54 -0700

    first p35

---
+++ src/Main.hx
@@ -0,0 +1,35 @@
+using StringTools;
+class Main {
+	static function main() 
+	{
+		new P35();
+	}
+}
+class P35
+{
+	/**
+	 * asked: Google
+	 * description: Given an array of strictly the characters 'R', 'G', and 'B', segregate the values of the array so that all the Rs come first, the Gs come second, and the Bs come last. You can only swap elements of the array.
+	 * requiriment: Do this in linear time and in-place.
+	 * example: given the array ['G', 'B', 'R', 'R', 'B', 'R', 'G'], it should become ['R', 'R', 'R', 'G', 'G', 'B', 'B'].
+	 */
+	public function new()
+	{
+		var array = ['G', 'B', 'R', 'R', 'B', 'R', 'G'];
+		array.sort(function(a:String,b:String)
+		{
+			return value(a) - value(b);
+		});
+		trace(array);
+	}
+	private function value(string:String):Int
+	{
+		return switch(string)
+		{
+			case "R": 0;
+			case "G": 1;
+			case "B": 2;
+			default: -1;
+		}
+	}
+}
